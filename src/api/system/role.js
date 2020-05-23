76f4f4488adc541fd06e0730deabb6b36caca29c
jumao <578961953@qq.com>
2020-05-24 03:00:17 +0800

    完成role管理，待完成用户角色分配

--- src/api/system/role.js
+++ src/api/system/role.js
@@ -2,38 +2,14 @@ import request from '@/utils/request'
 
 export function fetchList() {
   return request({
-    url: '/user/all',
+    url: '/role',
     method: 'get'
-  })
-}
-
-export function fetchDetail(id) {
-  return request({
-    url: '/user/detail',
-    method: 'get',
-    params: { id }
-  })
-}
-
-export function fetchPv(pv) {
-  return request({
-    url: '/vue-element-admin/article/pv',
-    method: 'get',
-    params: { pv }
-  })
-}
-
-export function create(data) {
-  return request({
-    url: '/user/create',
-    method: 'put',
-    data
   })
 }
 
 export function update(data) {
   return request({
-    url: '/user/status',
+    url: '/role',
     method: 'post',
     data
   })
--- src/views/system/role/index.vue
+++ src/views/system/role/index.vue
@@ -1,34 +1,6 @@
 <template>
   <div class="app-container">
-    <!-- <div style="margin-bottom:20px">
-      <el-button @click="adduser=!adduser">添加用户</el-button>
-      <el-drawer
-        ref="drawer"
-        title="添加用户"
-        :before-close="handleClose"
-        :visible.sync="addUser"
-        direction="ltr"
-        custom-class="demo-drawer"
-      >
-        <div>
-          <el-form :model="addUserForm">
-            <el-form-item label="活动名称" :label-width="formLabelWidth">
-              <el-input v-model="addUserForm.name" autocomplete="off" />
-            </el-form-item>
-            <el-form-item label="活动区域" :label-width="formLabelWidth">
-              <el-select v-model="addUserForm.region" placeholder="请选择活动区域">
-                <el-option label="区域一" value="shanghai" />
-                <el-option label="区域二" value="beijing" />
-              </el-select>
-            </el-form-item>
-          </el-form>
-          <div class="demo-drawer__footer">
-            <el-button @click="cancelForm">取 消</el-button>
-            <el-button type="primary" :loading="loading" @click="$refs.drawer.closeDrawer()">{{ loading ? '提交中 ...' : '确 定' }}</el-button>
-          </div>
-        </div>
-      </el-drawer>
-    </div> -->
+
     <el-table v-loading="listLoading" :data="list" border fit highlight-current-row style="width: 100%">
       <el-table-column align="center" label="ID" width="80">
         <template slot-scope="{row}">
@@ -36,82 +8,22 @@
         </template>
       </el-table-column>
 
-      <el-table-column width="180px" align="center" label="Email">
+      <el-table-column width="380px" align="center" label="RoleName">
         <template slot-scope="{row}">
-          <span>{{ row.email }}</span>
+          <span>{{ row.name }}</span>
         </template>
       </el-table-column>
 
-      <el-table-column width="120px" align="center" label="UserName">
-        <template slot-scope="{row}">
-          <span>{{ row.username }}</span>
-        </template>
-      </el-table-column>
-
-      <el-table-column width="500px" label="PassWord" align="center">
-        <template slot-scope="{row}">
-          <span>{{ row.password }}</span>
-        </template>
-      </el-table-column>
-
-      <el-table-column width="180px" label="Desc" align="center">
+      <el-table-column width="380px" align="center" label="Desc">
         <template slot-scope="{row}">
           <span>{{ row.desc }}</span>
         </template>
       </el-table-column>
 
-      <el-table-column class-name="status-col" label="Sex" width="80" align="center">
-        <template slot-scope="{row}">
-          <span>{{ row.sex===1 ?'男':'女' }}</span>
-        </template>
-      </el-table-column>
-
-      <el-table-column class-name="status-col" label="Age" width="80" align="center">
-        <template slot-scope="{row}">
-          <span>{{ row.age }}</span>
-        </template>
-      </el-table-column>
-
-      <el-table-column class-name="status-col" align="center" label="Avatar" width="110">
-        <template slot-scope="{row}">
-          <span>{{ row.avatar }}</span>
-        </template>
-      </el-table-column>
-
-      <el-table-column class-name="status-col" align="center" label="Phone" width="200">
-        <template slot-scope="{row}">
-          <span>{{ row.phone }}</span>
-        </template>
-      </el-table-column>
-
-      <el-table-column class-name="status-col" align="center" label="Ip" width="200">
-        <template slot-scope="{row}">
-          <span>{{ row.ip }}</span>
-        </template>
-      </el-table-column>
-
-      <el-table-column class-name="status-col" align="center" label="JoinTime" width="200">
-        <template slot-scope="{row}">
-          <span>{{ formateDate(row.createTime) }}</span>
-        </template>
-      </el-table-column>
-
-      <el-table-column class-name="status-col" align="center" label="UpdateTime" width="200">
-        <template slot-scope="{row}">
-          <span>{{ formateDate(row.updateTime) }}</span>
-        </template>
-      </el-table-column>
-
-      <el-table-column class-name="status-col" align="center" label="Status" width="200">
-        <template slot-scope="{row}">
-          <span>{{ row.delete===1?'禁用':'正常' }}</span>
-        </template>
-      </el-table-column>
-
-      <el-table-column class-name="status-col" align="center" label="Disable" width="200">
+      <el-table-column class-name="status-col" align="center" label="Disable" width="300px">
         <template slot-scope="{row}">
           <el-switch
-            v-model="row.disable"
+            v-model="row.delete"
             active-color="#13ce66"
             @change="statusChange(row)"
           />
@@ -123,7 +35,7 @@
 </template>
 
 <script>
-import { fetchList, update } from '@/api/resource/category'
+import { fetchList, update } from '@/api/system/role'
 
 export default {
   name: 'Category',
@@ -156,11 +68,11 @@ export default {
     async statusChange(row) {
       const { code, msg } = await update({
         id: row.id,
-        delete: row.delete === 1 ? 0 : 1
+        disable: row.disable === 1 ? 0 : 1
       })
       console.log(msg)
       if (code === 200) {
-        row.delete = row.delete === 1 ? 0 : 1
+        row.disable = row.disable === 1 ? 0 : 1
       } else {
         this.$message.error('service block')
       }
@@ -173,10 +85,9 @@ export default {
     async getList() {
       this.listLoading = true
       const { data } = await fetchList()
-      console.log(data)
       const items = data
       this.list = items.map(v => {
-        this.$set(v, 'disable', v.delete === 1) // https://vuejs.org/v2/guide/reactivity.html
+        this.$set(v, 'delete', v.disable === 1)
         return v
       })
       this.listLoading = false
